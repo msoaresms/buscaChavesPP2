cf72548916520af6c11ff1b70adb104189b80103
Matheus Soares <m.soares.ms@outlook.com>
2017-06-13 23:02:56 -0400

    o código já retorna as chaves colididades mas sem ordenar em ordem crescente

--- main.cpp
+++ main.cpp
@@ -5,6 +5,49 @@
 using namespace std;
 const int TAM = 787;
 //----------------------------------------------------------------------------------------------------------------------
+//adiciona zeros as chaves com menos de 4 caracteres
+int tamanho(string pLinha) {
+    int i = 0;
+    while (pLinha[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+string consertar(string pLinha){
+    int tam = tamanho(pLinha);
+    if (tam == 3){
+        char c1 = pLinha[0];
+        char c2 = pLinha[1];
+        char c3 = pLinha[2];
+        pLinha = "0";
+        pLinha += c1;
+        pLinha += c2;
+        pLinha += c3;
+    } else if (tam == 2){
+        char c1 = pLinha[0];
+        char c2 = pLinha[1];
+        pLinha = "00";
+        pLinha += c1;
+        pLinha += c2;
+    } else if (tam == 1){
+        char c1 = pLinha[0];
+        pLinha = "000";
+        pLinha += c1;
+    }
+    return pLinha;
+}
+//calcula o valor de hash
+int valorHash(string pLinha){
+    int hash = 0;
+    int j = 3;
+    for ( int i = 0; i < 4; i++){
+        int aux = pLinha[i];
+        hash = hash + (aux * pow(128, j));
+        j--;
+    }
+    return (hash % TAM);
+}
+//----------------------------------------------------------------------------------------------------------------------
 template <class T>
 class No{
 private:
@@ -133,95 +176,52 @@ public:
             this->tabela[i] = new Lista<T>();
         }
     }
-    void insere(int);
+    void insere(string);
     void remove(int);
-    void busca(int);
+    void busca(string);
 };
 
 template <typename T>
-void Hash<T>::insere(int pChave) {
-    Lista<T> *aux = tabela[pChave % TAM];
-    aux->insere(pChave);
+void Hash<T>::insere(string pChave) {
+    pChave = consertar(pChave);
+    int vHash = valorHash(pChave);
+    stringstream laux(pChave);
+    int nAux;
+    laux >> nAux;
+    Lista<T> *aux = tabela[vHash];
+    aux->insere(nAux);
 }
 template <typename T>
-void Hash<T>::busca(int pChave) {
-    Lista<T> *aux = tabela[pChave % TAM];
-    if (aux->busca(pChave)){
+void Hash<T>::busca(string pChave) {
+    int vHash = valorHash(pChave);
+    stringstream laux(pChave);
+    int nAux;
+    laux >> nAux;
+    Lista<T> *aux = tabela[vHash];
+    if (aux->busca(nAux)){
         aux->print();
     } else {
         printf("Chave n%co encontrada.", 198);
     }
 }
 //----------------------------------------------------------------------------------------------------------------------
-int tamanho(string pLinha) {
-    int i = 0;
-    while (pLinha[i] != '\0'){
-        i++;
-    }
-    return i;
-}
-string consertar(string pLinha){
-    int tam = tamanho(pLinha);
-    if (tam == 3){
-        char c1 = pLinha[0];
-        char c2 = pLinha[1];
-        char c3 = pLinha[2];
-        pLinha = "0";
-        pLinha += c1;
-        pLinha += c2;
-        pLinha += c3;
-    } else if (tam == 2){
-        char c1 = pLinha[0];
-        char c2 = pLinha[1];
-        pLinha = "00";
-        pLinha += c1;
-        pLinha += c2;
-    } else if (tam == 1){
-        char c1 = pLinha[0];
-        pLinha = "000";
-        pLinha += c1;
-    }
-    return pLinha;
-}
-//----------------------------------------------------------------------------------------------------------------------
 int main() {
-//    Hash<int> tabela;
-//
-//    ifstream arquivo;
-//    arquivo.open("chaves.txt");
-//
-//    if (!arquivo){
-//        cout << "NUM PRESTOU" << endl;
-//    } else {
-//        string chave;
-//        while (arquivo >> chave){
-//            stringstream aux(chave);
-//            int nAux;
-//            aux >> nAux;
-//            tabela.insere(nAux);
-//        }
-//    }
-//    arquivo.close();
-//
-//    tabela.busca(1000);
+    Hash<int> tabela;
 
-//    string teste = "9808";
-//    int tam = tamanho(teste);
-//    int asc = 0;
-//    int j = 3;
-//    for ( int i = 0; i < tam; i++){
-//        int aux = teste[i];
-//        asc = asc + (aux * pow(128, j));
-//        j--;
-//    }
-//
-//    cout << asc;
+    ifstream arquivo;
+    arquivo.open("chaves.txt");
 
-    string teste = "1";
-    teste = consertar(teste);
+    if (!arquivo){
+        cout << "NUM PRESTOU" << endl;
+    } else {
+        string chave;
+        while (arquivo >> chave){
+            tabela.insere(chave);
+        }
+    }
+    arquivo.close();
 
-    cout << teste;
-
+    tabela.busca("9808");
 
     return 0;
 }
